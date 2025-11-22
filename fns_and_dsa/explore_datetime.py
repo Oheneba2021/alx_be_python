adc458b8edffbeeabf1ec11053c3c7920ef67b7f
Godwin Boakye <ohenebayb@gmail.com>
2025-11-22 16:21:15 +0000

    explore datetime review fixes

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,12 +4,12 @@ def display_current_datetime():
     now = datetime.now()
     # formatted_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current Date and Time: {now}")
-# current_datetime = datetime.now()
+current_datetime = datetime.now()
 display_current_datetime()
 days_ahead = int(input("Enter the number of days to add to the current date:"))
 def future_date(days_ahead):
     """Returns the date that is 'days_ahead' days from today."""
-    future = current_datetime + datetime.timedelta(days=days_ahead)
+    calculate_future_date = current_datetime + datetime.timedelta(days=days_ahead)
     return future
 
 print(f'future date: {future_date(days_ahead)}')
